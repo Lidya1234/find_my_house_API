b56e693b51a5f138ad4a13f7a96f84baaa328c26
Lidya1234 <lidya94gher@gmail.com>
2021-05-17 23:25:29 +0300

    Add the api house controller

---
+++ app/controllers/api/v1/houses_controller.rb
@@ -0,0 +1,39 @@
+module Api
+    module V1
+        class HousesController < ApplicationController
+            def index 
+                @houses = House.all
+                render json: {status: 'SUCCESS', message: 'Loaded houses', data: @houses},status: :ok
+            end
+            def show
+                @house = House.find(params[:id])
+                render json: {status: 'SUCCESS', message: 'Loaded house', data: @house},status: :ok
+            end
+            def create
+                @house = House.new(house_params)
+                if @house.save
+                    render json: {status: 'SUCCESS', message: 'Saved house', data: @house},status: :ok
+                else
+                    render json: {status: 'ERROR', message: 'house not saved', data: @house.errors},status: :unprocessable_entry
+                end
+            end
+            def destroy
+                @house = House.find(params[:id])
+                @house.destroy
+                render json: {status: 'SUCCESS', message: 'Deleted house', data: @house},status: :ok
+            end
+            def update
+                @house = House.find(params[:id])
+                if @house.update_attributes(house_params)
+                    render json: {status: 'SUCCESS', message: 'Updated house', data: @house},status: :ok
+                else
+                    render json: {status: 'ERROR', message: 'house not saved', data: @house.errors},status: :unprocessable_entry
+                end
+            end
+            private
+            def house_params
+                params.permit(:name, :rank, :image, :price)
+            end
+        end
+    end
+end
