83ffb3613e5bc2aa61606dbd94f7c2f045842d52
Lidya1234 <lidya94gher@gmail.com>
2021-06-02 13:13:54 +0300

    Add routes to favorite

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   resources :users, only: [:create, :show, :index] do 
     resources :items, only: [:create, :show, :index, :destroy]
  end
+ resources :favorites, only: [:create, :show, :index] 
 end
