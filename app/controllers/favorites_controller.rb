f156f45b6036f1f094bc2d3e0fd828790373642e
Lidya1234 <lidya94gher@gmail.com>
2021-06-02 11:35:14 +0300

    Add entries to the seed

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,35 @@
+class FavoritesController < ApplicationController
+    def index 
+        @favorites = Favorite.all
+        render json: {status: 'SUCCESS', message: 'Loaded favorites', data: @favorites},status: :ok
+    end
+    def show
+        @favorite = Favorite.find(params[:id])
+        render json: {status: 'SUCCESS', message: 'Loaded favorite', data: @favorite},status: :ok
+    end
+    def create
+        @favorite = Favorite.new(favorite_params)
+        if @favorite.save
+            render json: {status: 'SUCCESS', message: 'Saved favorite', data: @favorite},status: :ok
+        else
+            render json: {status: 'ERROR', message: 'favorite not saved', data: @favorite.errors},status: :unprocessable_entry
+        end
+    end
+    def destroy
+        @favorite = Favorite.find(params[:id])
+        @favorite.destroy
+        render json: {status: 'SUCCESS', message: 'Deleted favorite', data: @favorite},status: :ok
+    end
+    def update
+        @favorite = Favorite.find(params[:id])
+        if @favorite.update_attributes(favorite_params)
+            render json: {status: 'SUCCESS', message: 'Updated favorite', data: @favorite},status: :ok
+        else
+            render json: {status: 'ERROR', message: 'favorite not saved', data: @favorite.errors},status: :unprocessable_entry
+        end
+    end
+    private
+    def favorite_params
+        params.permit(:user_id, :house_id)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_26_153915) do
+ActiveRecord::Schema.define(version: 2021_06_01_150000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "house_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["house_id"], name: "index_favorites_on_house_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "favourites", force: :cascade do |t|
     t.string "name"
@@ -31,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_05_26_153915) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
+    t.string "description"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +50,6 @@ ActiveRecord::Schema.define(version: 2021_05_26_153915) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "favorites", "houses"
+  add_foreign_key "favorites", "users"
 end
