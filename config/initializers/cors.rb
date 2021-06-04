17b59e2c20360ccfeb4fd3fa466f3ce83b13e0d4
Lidya1234 <lidya94gher@gmail.com>
2021-06-04 19:49:54 +0300

    Change cors links

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://findlidushousereact.herokuapp.com', 'http://localhost:3001'
+    origins '*'
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-      credentials: true
   end
 end
