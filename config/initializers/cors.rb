14f8162b94fb02100f293953d5bc51780d6a52ac
Lidya1234 <lidya94gher@gmail.com>
2021-06-04 13:31:59 +0300

    Add react link

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://findlidushouse.herokuapp.com', 'http://localhost:3001'
+    origins 'https://findlidushousereact.herokuapp.com', 'http://localhost:3001'
 
     resource '*',
       headers: :any,
