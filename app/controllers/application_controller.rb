b0eea9b135b3d05fb69cc6813d987ad49c15379a
Lidya1234 <lidya94gher@gmail.com>
2021-06-07 20:09:33 +0300

    Remove current user from is_logged_in method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ def login!
     session[:user_id] = @user.id
 end
 def logged_in?
-    !session[:user_id].nil?
+    !!session[:user_id]
 end
 def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
