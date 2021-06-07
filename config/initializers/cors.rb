0b7e4ab0264462e40862b63d96e282ccfdc6d515
Lidya1234 <lidya94gher@gmail.com>
2021-06-07 13:37:25 +0300

    Remoe the post resources

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,19 +4,14 @@
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
 # Read more: https://github.com/cyu/rack-cors
-Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true, logger: (-> { Rails.logger }) do
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-
-    resource '/cors',
-      :headers => :any,
-      :methods => [:post],
-      :max_age => 0
-
+    origins '*',
     resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      :max_age => 0
+          headers: :any,
+          methods: %i[get post put patch delete options head],
+        
   end
 end
 
