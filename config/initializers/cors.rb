72895474c56b09c164714986c1c268692dbc9867
Lidya1234 <lidya94gher@gmail.com>
2021-06-07 13:23:24 +0300

    Add logger to cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,14 +4,19 @@
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
 # Read more: https://github.com/cyu/rack-cors
-
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
+Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true, logger: (-> { Rails.logger }) do
   allow do
-    origins '*',
+    origins '*'
+
+    resource '/cors',
+      :headers => :any,
+      :methods => [:post],
+      :max_age => 0
+
     resource '*',
-          headers: :any,
-          methods: %i[get post put patch delete options head],
-        
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      :max_age => 0
   end
 end
 
