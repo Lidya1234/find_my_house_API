6cb5aaa044400ceb7b0aa17af171090ae9651af9
Lidya1234 <lidya94gher@gmail.com>
2021-06-07 19:16:12 +0300

    Change user contoller file

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     @user = User.find_by(username: session_params[:username])
-
+  
     if @user && @user.authenticate(session_params[:password])
       login!
       render json: {
@@ -9,14 +9,13 @@ class SessionsController < ApplicationController
         user: @user
       }
     else
-      render json: {
+      render json: { 
         status: 401,
-        errors: ['login failed']
+        errors: ['no such user, please try again']
       }
     end
-  end
-
-  def is_logged_in?
+end
+def is_logged_in?
     if logged_in? && current_user
       render json: {
         logged_in: true,
@@ -28,19 +27,16 @@ class SessionsController < ApplicationController
         message: 'no such user'
       }
     end
-  end
-
-  def destroy
-    logout!
-    render json: {
-      status: 200,
-      logged_out: true
-    }
-  end
-
-  private
-
-  def session_params
-    params.require(:user).permit(:username, :password)
-  end
+end
+def destroy
+      logout!
+      render json: {
+        status: 200,
+        logged_out: true
+      }
+end
+private
+def session_params
+      params.require(:user).permit(:username, :password)
+end
 end
