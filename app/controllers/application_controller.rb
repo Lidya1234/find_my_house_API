ee365e4be96a7a986d884b6bc4877b3335893c65
Lidya1234 <lidya94gher@gmail.com>
2021-06-07 19:24:11 +0300

    Add .nil? to logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ def login!
     session[:user_id] = @user.id
 end
 def logged_in?
-    !!session[:user_id]
+    !session[:user_id].nil?
 end
 def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
