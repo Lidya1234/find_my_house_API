4a4c4ec45970327ebb71f13acd8960bec7bdb0da
Lidya1234 <lidya94gher@gmail.com>
2021-06-07 22:08:13 +0300

    update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
-if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_find_my_house_API', domain: 'find_my_house_API-json-api'
-  else
-    Rails.application.config.session_store :cookie_store, key: '_find_my_house_API'
-  end    
+if Rails.env === 'production'
+  Rails.application.config.session_store :cookie_store, key: '_api_trackyourreading_session', domain: '*'
+else
+  Rails.application.config.session_store :cookie_store, key: '_api_trackyourreading_session'
+end   
 
   # if Rails.env === 'production' 
   #   Rails.application.config.session_store :cookie_store, key: '_find_my_house_API', domain: 'find_my_house_API-json-api'
