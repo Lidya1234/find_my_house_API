afc881373edf98a7df3bd3e9fdddb6b0e9c33fb2
Lidya1234 <lidya94gher@gmail.com>
2021-06-07 10:58:22 +0300

    Add allow oringin *

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
-
+    origins 'http://localhost:3000',
+    Access-Control-Allow-Origin: *
     resource '*',
           headers: :any,
           methods: %i[get post put patch delete options head],
