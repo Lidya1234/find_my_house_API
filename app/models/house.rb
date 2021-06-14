f036154faf0aa6d77bd0e61453e5ef782381a3f7
Lidya1234 <lidya94gher@gmail.com>
2021-06-14 12:13:56 +0300

    Fix linter errors

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,5 +1,5 @@
 class House < ApplicationRecord
-  has_many :favorites, class_name: "Favorite", foreign_key: "house_id"
+  has_many :favorites, class_name: 'Favorite', foreign_key: 'house_id'
   validates :name, presence: true
   validates :rank, presence: true
   validates :image, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :favorites, class_name: "Favorite", foreign_key: "user_id"
+  has_many :favorites, class_name: 'Favorite', foreign_key: 'user_id'
   validates :username, presence: true
   validates :username, uniqueness: true
   validates :username, length: { minimum: 4 }
