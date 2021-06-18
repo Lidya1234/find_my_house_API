24ad8fa19018132d2d05d0f0716814fade9bb245
Lidya1234 <lidya94gher@gmail.com>
2021-06-18 11:47:04 +0300

    Change destroy argument to to take user_id instead of id

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -20,7 +20,7 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
-    @favorite = Favorite.find(params[:id])
+    @favorite = Favorite.find(params[:user_id])
     @favorite.destroy
     render json: { status: 'SUCCESS', message: 'Deleted favorite', data: @favorite }, status: :ok
   end
